a01282f4b3ceb242061310dc217495cbb99c3104
Daniel-Rosbak <rosbak.daniel@gmail.com>
2025-12-06 17:34:08 +0100

    completed Day 6 part 2

--- Day6.hs
+++ Day6.hs
@@ -1,24 +1,37 @@
 
-part2 s = print (ls)
+part2 s = print (p2solveproblems problems)
             where
-                --problems = p2makenums w
-                --w = (customwords (take 4 ls))++(words(drop 4 ls))
-                ls = lines s
-{-
-p2makenums x = ([map str2int a | a <- take 4 x], head (drop 4 x))
+                problems = p2makenums w
+                w = (customwords (take 4 ls))++(map words (drop 4 ls))
+                ls = map (++ " ") (lines s)
+
+p2solveproblems (_,[]) = 0
+p2solveproblems (a,b) = if op == "+"
+                    then
+                        (sum probs) + d
+                    else
+                        (foldr (*) 1 (filter (\y -> not(0 == y)) probs)) + d
+                    where
+                        op = head b
+                        probs = map head a
+                        d = p2solveproblems (map tail a, tail b)
+
+p2makenums x = ([map (\s -> if (s == "") then 0 else str2int s) a | a <- take 4 x], head(drop 4 x))
 
 customwords [[],[],[],[]] = [[],[],[],[]]
-customwords [a,b,c,d] = [x:xd,y:yd,x:xd,w:wd]
+customwords [a,b,c,d] = [x:xd,y:yd,z:zd,w:wd]
                         where
-                            (xd,yd,zd,wd) = customwords p
-                            [x,y,z,w] = (transpose t)
+                            [xd,yd,zd,wd] = customwords p
+                            [x,y,z,w] = transpose t
                             p = [drop (s+1) a, drop (s+1) b, drop (s+1) c, drop (s+1) d]
                             t = (take s a, take s b, take s c, take s d)
                             s = (head [n | n <- [0..4], (a!!n) == ' ' && (b!!n) == ' ' && (c!!n) == ' ' && (d!!n) == ' '])
 
-transpose ([],[],[],[]) = []
-transpose ((a:as),(b:bs),(c:cs),(d:ds)) = (a:b:c:d):transpose(as,bs,cs,ds)
--}
+transpose (as,bs,cs,ds) = (replicate (4-(length p)) "")++p
+                        where
+                            p = foldr (++) [] (map words [[as!!n,bs!!n,cs!!n,ds!!n] | n <- [0..((length as)-1)]])
+
+
 part1 s = print (solveproblems problems)
             where
                 problems = makenums w
@@ -43,3 +56,5 @@ str2int (x:xs) = ((fromEnum x) - 48) * (10 ^ (length xs)) + str2int xs
 
 
 -- input goes here:
+testinput = ""
+getinput = ""
---
+++ Day7.hs
@@ -0,0 +1,10 @@
+
+
+part1 s = print (ls)
+            where
+                ls = lines s
+
+
+-- input goes here:
+testinput = ""
+getinput = ""
